e9f339d2f4a69003e51ce42b0abbc3f7f6660245
baiiu <baiiu@foxmail.com>
2017-11-20 23:18:12 +0800

    python

--- packAndroid.py
+++ packAndroid.py
@@ -37,8 +37,8 @@ assembleRelease = cf.getboolean('app','assembleRelease')
 # 1.设置目录
 base_file_dir = cf.get('dir','base_file_dir')
 create_dir_name = cf.get('dir','create_dir_name')
-create_code_dir_name = cf.get('dir','create_code_dir_name')
-create_apk_dir_name = cf.get('dir','create_apk_dir_name')
+create_code_dir_name = cf.get('dir','create_code_dir_name') #源码路径
+create_apk_dir_name = cf.get('dir','create_apk_dir_name') # apk路径
 
 file_dir = base_file_dir + '/' + create_dir_name    #/Users/baiiu/Desktop/AndroidApp
 code_dir = file_dir + '/' + create_code_dir_name      #/Users/baiiu/Desktop/AndroidApp/SourceCode
@@ -158,75 +158,3 @@ source_apk_dir = code_dir + '/' + 'app/build/outputs/apk'
 
 if os.path.exists(source_apk_dir):
     moveFiles(source_apk_dir,apk_dir)
-
-
-##########################################################################################
-#6. 安装apk
-print('\n')
-print('=============================================')
-print('install apk')
-print('=============================================')
-
-print ('现在所在位置： ' + os.getcwd())
-
-
-def getFileName(sourceDir):
-    if not os.path.exists(sourceDir):
-        return;
-
-    for filename in os.listdir(sourceDir):
-        if '.apk' in filename:
-            return filename
-
-
-
-apkName = getFileName(apk_dir)
-apkPath = apk_dir + '/' + apkName
-print('apkPath: '+ apkPath)
-
-os.system('adb install -r ' + apkPath)
-
-#########################################################################################
-# 7.打开APK
-def cmp(a, b):
-    return (a > b) - (a < b)
-
-#获取PackageName,launcherActivity
-def findLauncherActivityName(ele,targetString):
-    for childElem in ele:
-        if len(list(childElem)) == 0:
-            if cmp(childElem.get('{http://schemas.android.com/apk/res/android}name'),"'" + targetString +"'"):
-                return True
-            else:
-                return False
-        else:
-            # print('has child, ' + childElem.tag, childElem.attrib)
-            return findLauncherActivityName(childElem,targetString)
-
-def getLauncherActivity(xmlFileDir):
-    tree = ET.ElementTree(file=xmlFileDir)
-
-    root = tree.getroot()
-    packageName = root.get('package')
-
-    for elem in tree.iter(tag = 'activity'):
-        if findLauncherActivityName(elem,'android.intent.action.MAIN'): #if has
-            if(findLauncherActivityName(elem,'android.intent.category.LAUNCHER')):
-                return packageName,elem.get('{http://schemas.android.com/apk/res/android}name')
-
-    return 'NULL'
-
-PackageName,LauncherActivity = getLauncherActivity(code_dir + '/app/src/main/AndroidManifest.xml')
-
-print (PackageName + '  ' + LauncherActivity)
-
-# PackageName = cf.get('app','PackageName')
-# LauncherActivity = cf.get('app','LauncherActivity')
-
-# 先关闭该进程
-os.system('adb shell am force-stop ' + PackageName)
-# 打开该LauncherActivity
-if PackageName in LauncherActivity:
-    os.system('adb shell am start -n '+ PackageName +'/' + LauncherActivity)
-else:
-    os.system('adb shell am start -n '+ PackageName +'/' + PackageName + LauncherActivity)
