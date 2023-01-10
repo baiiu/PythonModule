7ff602c458afcd3f4eee84cb51e15733a1d78066
zhuzhe <zhuzhe.zz@bytedance.com>
2023-01-10 21:36:15 +0800

    gc

--- logcat_gc.py
+++ logcat_gc.py
@@ -3,22 +3,42 @@
 
 import os
 import subprocess
+import sys
 
+count = 0
+allocCount = 0
 
-def isGC(log):
+def isGC(line, pid):
     try:
-        line = str(log, encoding="utf-8").lower()
-        return ("dalvikvm" in line or "art" in line) and "gc" in line and "freed" in line and "paused" in line
+        if (len(pid) == 0):
+            return ("dalvikvm" in line or "art" in line) and "gc" in line and "freed" in line and "paused" in line
+        else:
+            return ("dalvikvm" in line or "art" in line) and "gc" in line and "freed" in line and "paused" in line and pid in line
     except Exception as e:
         return False
 
-def checkLogcat():
+def isAlloc(line):
+    try:
+        return "gc_for_alloc" in line
+    except Exception as e:
+        return False
+
+def checkLogcat(pid):
+    global count
+    global allocCount
     cmd = "adb logcat -v time"
     logcat = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) # 使用管道
     while not logcat.poll():
         log = logcat.stdout.readline()
-        if(isGC(log)):
-            print(log.strip())
+        line = str(log, encoding="utf-8").lower()
+        if(isGC(line, pid)):
+            count += 1
+            if(isAlloc(line)):
+                allocCount += 1
+            print("all: " + str(count) + " , alloc:" + str(allocCount) + " , " + str(log, encoding="utf-8").strip())
 
 if __name__ == '__main__':
-    checkLogcat()
+    if(len(sys.argv) == 1):
+        checkLogcat("")
+    else:
+        checkLogcat(sys.argv[1])
